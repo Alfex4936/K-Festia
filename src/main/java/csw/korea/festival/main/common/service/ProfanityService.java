f88c5c662948394d375a03f6f516ccde3d3968da
Alfex4936 <ikr@kakao.com>
2024-09-29 12:58:04 +0900

    Feat: Profanity Service

---
+++ src/main/java/csw/korea/festival/main/common/service/ProfanityService.java
@@ -0,0 +1,56 @@
+package csw.korea.festival.main.common.service;
+
+
+import csw.korea.festival.main.util.DAT;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeMap;
+
+@Slf4j
+@Service
+public class ProfanityService {
+    private DAT<String> trie;
+    @Getter
+    private List<String> badWords;
+
+    @PostConstruct
+    public void init() throws IOException {
+        trie = new DAT<>();
+        TreeMap<String, String> badWordsMap = new TreeMap<>();
+
+        // Load bad words from file
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(getClass().getResourceAsStream("/badwords.txt")), StandardCharsets.UTF_8))) {
+            badWords = reader.lines()
+                    .filter(word -> !word.isEmpty())
+                    .distinct().toList();
+            for (String word : badWords) {
+                badWordsMap.put(word, word);
+            }
+        }
+
+        trie.build(badWordsMap); // thread-safe after
+
+        assert (trie != null): "Trie should not be null.";
+    }
+
+    public boolean containsProfanity(String text) {
+//        List<AhoCorasickDoubleArrayTrie.Hit<String>> hits = trie.parseText(text);
+        DAT.Hit<String> hit = trie.findFirst(text);
+        if (hit == null) {
+            return false;
+        }
+
+        return hit.begin() >= 0 && hit.end() >= 0;
+    }
+
+}
